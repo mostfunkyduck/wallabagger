b35811202d6df036dc0da33dfd1765438dfb2d64
funkyduck <e@m.ail>
2021-03-21 04:59:27 -0400

    removing console.log

--- wallabagger/js/content.js
+++ wallabagger/js/content.js
@@ -4,7 +4,6 @@ if (typeof (browser) === 'undefined' && typeof (chrome) === 'object') {
 
 browser.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(`got request in content.js: ${request}`)
     if( request.getContent ) {
       sendResponse({content: document.body.outerHTML})
     }
